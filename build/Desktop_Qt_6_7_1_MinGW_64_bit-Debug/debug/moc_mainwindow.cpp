6e7f41813beb5b30dd1b74d6e4fd784e44f265fb
Tsalvador03 <tomsalva2003@gmail.com>
2024-08-11 20:30:55 +0200

    ajout des images

---
+++ build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,211 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "calculePrix",
+    "",
+    "std::vector<Alcool>",
+    "std::vector<Dilluant>",
+    "listRecipes",
+    "onSearchButtonClicked",
+    "clearWidget",
+    "QWidget*",
+    "widget",
+    "initialisation",
+    "onImageClicked",
+    "std::string",
+    "recipeName",
+    "onAjouterClicked",
+    "reorganizeWidgets",
+    "widgetToRemove",
+    "rechercheAvecFiltre",
+    "clearFiltre"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      10,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    2,   74,    2, 0x08,    1 /* Private */,
+       5,    0,   79,    2, 0x08,    4 /* Private */,
+       6,    0,   80,    2, 0x08,    5 /* Private */,
+       7,    1,   81,    2, 0x08,    6 /* Private */,
+      10,    0,   84,    2, 0x08,    8 /* Private */,
+      11,    1,   85,    2, 0x08,    9 /* Private */,
+      14,    0,   88,    2, 0x08,   11 /* Private */,
+      15,    1,   89,    2, 0x08,   12 /* Private */,
+      17,    0,   92,    2, 0x08,   14 /* Private */,
+      18,    0,   93,    2, 0x08,   15 /* Private */,
+
+ // slots: parameters
+    QMetaType::Double, 0x80000000 | 3, 0x80000000 | 4,    2,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 8,    9,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 12,   13,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 8,   16,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'calculePrix'
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<std::vector<Alcool>, std::false_type>,
+        QtPrivate::TypeAndForceComplete<std::vector<Dilluant>, std::false_type>,
+        // method 'listRecipes'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onSearchButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'clearWidget'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QWidget *, std::false_type>,
+        // method 'initialisation'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'onImageClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const std::string &, std::false_type>,
+        // method 'onAjouterClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'reorganizeWidgets'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QWidget *, std::false_type>,
+        // method 'rechercheAvecFiltre'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'clearFiltre'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: { double _r = _t->calculePrix((*reinterpret_cast< std::add_pointer_t<std::vector<Alcool>>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<std::vector<Dilluant>>>(_a[2])));
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = std::move(_r); }  break;
+        case 1: _t->listRecipes(); break;
+        case 2: _t->onSearchButtonClicked(); break;
+        case 3: _t->clearWidget((*reinterpret_cast< std::add_pointer_t<QWidget*>>(_a[1]))); break;
+        case 4: _t->initialisation(); break;
+        case 5: _t->onImageClicked((*reinterpret_cast< std::add_pointer_t<std::string>>(_a[1]))); break;
+        case 6: _t->onAjouterClicked(); break;
+        case 7: _t->reorganizeWidgets((*reinterpret_cast< std::add_pointer_t<QWidget*>>(_a[1]))); break;
+        case 8: _t->rechercheAvecFiltre(); break;
+        case 9: _t->clearFiltre(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+        case 3:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< QWidget* >(); break;
+            }
+            break;
+        case 7:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< QWidget* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    }
+    return _id;
+}
+QT_WARNING_POP
